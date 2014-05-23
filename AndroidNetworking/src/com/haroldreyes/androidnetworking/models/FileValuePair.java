f79b933314115b3dcaacf5bf61fe1c11a4387419
Harold Reyes <haroldreyes@Harolds-Mac-mini.local>
2014-05-23 10:12:39 +0800

    First Commit

---
+++ AndroidNetworking/src/com/haroldreyes/androidnetworking/models/FileValuePair.java
@@ -0,0 +1,39 @@
+/*
+ * 
+ * Copyright (C) 2014 CodeMagnus. All Rights Reserved.
+ * Created by Edgar Harold C. Reyes
+ * eharoldreyes@gmail.com
+ * 
+ */
+package com.haroldreyes.androidnetworking.models;
+
+import java.io.File;
+
+public class FileValuePair {
+
+	private File file;
+	private String name;
+
+	public FileValuePair(String name, File file){
+		this.setFile(file);
+		this.setName(name);
+	}
+	
+	
+	public File getFile() {
+		return file;
+	}
+
+	public void setFile(File file) {
+		this.file = file;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
