a65943cfc1d71f3d65998cc3db54f1a36a3da6c8
Harold Reyes <haroldreyes@Harolds-Mac-mini.local>
2014-05-23 16:36:40 +0800

    Added GZip Support

--- AndroidNetworking Sample/src/com/haroldreyes/sample/MainActivity.java
+++ AndroidNetworking Sample/src/com/haroldreyes/sample/MainActivity.java
@@ -5,33 +5,51 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.haroldreyes.androidnetworking.WebserviceRequest;
+import com.haroldreyes.androidnetworking.WebserviceRequest.HttpGET;
 
 public class MainActivity extends Activity {
+
+	private HttpGET httpGet;
+	private HttpGET httpGETUnGzipped;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		//Creating a new instance of the request.
-	    WebserviceRequest.HttpGET httpGet = new WebserviceRequest.HttpGET(); 
+		
+	    httpGet = new HttpGET(); //Creating a new instance of the request.	    
+	    httpGet.setUrl("http://stage.jualo.com/users/119394.json"); //Setting the URL 
+	    httpGet.setCallback(callBack); //Setting the Callback
+	    httpGet.execute(); //Executing the request
+	    	    
+	    httpGETUnGzipped = new HttpGET();
+	    httpGETUnGzipped.setUrl("https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow");
+	    httpGETUnGzipped.setCallback(unGZippedCallback);
+	    httpGETUnGzipped.execute();
 	    
-	    //Setting the URL 
-	    httpGet.setUrl("http://ip.jsontest.com/");
-	    
-	    //Setting the Callback
-	    httpGet.setCallback(callBack);
-
-	    //Executing the request
-	    httpGet.execute();
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if(httpGet != null) httpGet.cancel();		
+		if(httpGETUnGzipped != null) httpGETUnGzipped.cancel();
 	}
 	
 	private WebserviceRequest.Callback callBack = new WebserviceRequest.Callback() {
 	    @Override
 	    public void onResult(int responseCode, String responseMessage, Exception exception) {
 	        if(responseCode == 200 && exception == null){           
-	        	//Getting the response from callback
 	            Log.d("callback", "responseMessage: " + responseMessage);   
+	        }               
+	    }               
+	};
+	
+	private WebserviceRequest.Callback unGZippedCallback = new WebserviceRequest.Callback() {
+	    @Override
+	    public void onResult(int responseCode, String responseMessage, Exception exception) {
+	        if(responseCode == 200 && exception == null){           
+	            Log.i("unGZippedCallback", "responseMessage: " + responseMessage);   
 	        }               
 	    }               
 	};
